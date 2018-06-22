72f7d21f73f1d9e0c59e904d6582fef2aaab2717
Juan Carlos Galan Hernandez <jcgalanh@gmail.com>
2018-06-21 19:45:18 -0500

    This closes #3

--- src/main/java/com/juancgalan/hi_lo/HiLo.java
+++ src/main/java/com/juancgalan/hi_lo/HiLo.java
@@ -1,10 +1,15 @@
 package com.juancgalan.hi_lo;
 
+import java.util.Scanner;
+
 public class HiLo {
   public static void main(String... args) {
     double f = Math.random() / Math.nextDown(1.0);
     int number = (int) (100 * f);
+    Scanner scan  = new Scanner(System.in);
+    int guess = 0;
     System.out.println("Guess a number between 1 and 100");
+    guess = scan.nextInt();
 
   }
 }
