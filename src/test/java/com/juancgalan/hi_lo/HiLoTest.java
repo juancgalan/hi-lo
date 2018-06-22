6c470b77496bb5c27bcf1e95e3451de1f66cd34b
Juan Carlos Galan Hernandez <jcgalanh@gmail.com>
2018-06-21 20:00:28 -0500

    This closes #9

--- src/test/java/com/juancgalan/hi_lo/HiLoTest.java
+++ src/test/java/com/juancgalan/hi_lo/HiLoTest.java
@@ -1,5 +1,32 @@
 package com.juancgalan.hi_lo;
 
-public class HiLoTest {
+import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class HiLoTest {
+  @Test
+  public void checkGuessShouldDetectLower() {
+    HiLo hilo = new HiLo();
+    String expected = "It's too low.";
+    String actual = hilo.checkGuess(3, 5);
+    assertThat(actual, equalTo(expected));
+  }
+
+  @Test
+  public void checkGuessShouldDetectHigher() {
+    HiLo hilo = new HiLo();
+    String expected = "It's too high.";
+    String actual = hilo.checkGuess(7, 5);
+    assertThat(actual, equalTo(expected));
+  }
+
+  @Test
+  public void checkGuessShouldDetectCorrect() {
+    HiLo hilo = new HiLo();
+    String expected = "It's correct!";
+    String actual = hilo.checkGuess(7, 5);
+    assertThat(actual, equalTo(expected));
+  }
 }
