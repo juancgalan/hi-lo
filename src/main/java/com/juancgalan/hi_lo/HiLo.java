6f86427fbe801a1e97dfcab9a14a557acb7e2023
Juan Carlos Galan Hernandez <jcgalanh@gmail.com>
2018-06-21 19:42:56 -0500

    This closes #2 and #7

--- src/main/java/com/juancgalan/hi_lo/HiLo.java
+++ src/main/java/com/juancgalan/hi_lo/HiLo.java
@@ -2,6 +2,9 @@ package com.juancgalan.hi_lo;
 
 public class HiLo {
   public static void main(String... args) {
+    double f = Math.random() / Math.nextDown(1.0);
+    int number = (int) (100 * f);
     System.out.println("Guess a number between 1 and 100");
+
   }
 }
